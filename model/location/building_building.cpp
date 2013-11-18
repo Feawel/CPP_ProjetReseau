6b617b23b292ab282bb5cd76e91dc3eb2ae9d69e
Govipi <vincent_moulin@hotmail.fr>
2013-11-18 12:05:15 +0100

    Modification b2b
    Ajout de la visibilité
    Ajout des installations existantes

--- model/location/building_building.cpp
+++ model/location/building_building.cpp
@@ -7,7 +7,22 @@
 #include "building_building.h"
 
 Building_Building::Building_Building(Building *building1, Building *building2,
-		float distance) :
-		building1(building1), building2(building2), distance(distance) {
+		float distance, bool visibility) :
+		building1(building1), building2(building2), distance(distance), visibility(
+				visibility) {
 }
 
+/**
+ * Add a existing technology between 2 buildings
+ */
+void Building_Building::addExistingTechnology(Technology technology) {
+	existingTechs.push_back(technology);
+}
+
+float Building_Building::getDistance() {
+	return distance;
+}
+
+std::vector<Technology> Building_Building::getExistingTechs() {
+	return existingTechs;
+}
--- model/location/building_building.h
+++ model/location/building_building.h
@@ -9,17 +9,23 @@
 #define BUILDING_BUILDING_H_
 
 #include "building.h"
+#include "../constant.h"
+#include <vector>
 
 class Building_Building {
 
 public:
-	Building_Building(Building *building1, Building *building2, float distance);
+	Building_Building(Building *building1, Building *building2, float distance,bool visibility);
+	void addExistingTechnology(Technology technology);
+	float getDistance();
+	std::vector<Technology> getExistingTechs();
 
 private:
 	float distance;
 	Building *building1;
 	Building *building2;
-
+	bool visibility;
+	std::vector<Technology> existingTechs;
 };
 
 #endif /* BUILDING_BUILDING_H_ */
