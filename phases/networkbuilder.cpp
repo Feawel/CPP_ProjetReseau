874dc94b132b0131bb79de974bc3ff699315080e
Feawel <banon.jonathan01@gmail.com>
2013-11-23 16:31:47 +0100

    Squelette Phase 2

--- phases/networkbuilder.cpp
+++ phases/networkbuilder.cpp
@@ -68,14 +68,66 @@ void NetworkBuilder::launchP1(Request request) {
     }
 }
 
-void NetworkBuilder::launchP2(Building_Building B2B) {
+void NetworkBuilder::launchP2(vector<Building_Building> B2B) {
+    /*
+     *Phase II : Technologies de liaison inter-batiments
+        Définition des distances B2B
+        Fibre optique (défaut)
+        Paire torsadée (SI paire torsadée déjà câblée entre bâtiments)
+        InfraRouge (D(B2B) < 3 && vision == true)
+        Pont Wifi (D(B2B) < 0,5 && vision == true)
+
+     */
+
+    int i;
+    for (i=0;i<B2B.size();i++){
+
+        if (B2B[i].getDistance() < 0.5 && B2B[i].getVisibility() == true) {
+                B2B[i].addExistingTechnology(NTechnology::WIFI);
+
+            }
+            else if (B2B[i].getDistance() < 3 && B2B[i].getVisibility() == true){
+                B2B[i].addExistingTechnology(NTechnology::INFRARED);
+            }
+            else {
+                B2B[i].addExistingTechnology(NTechnology::FIBER);
+            }
+
+
+    }
+
+
 
 }
 
 void NetworkBuilder::launchP3(Request request) {
+    /*
+     *Phase III : Liaisons intra-batiment
+        Création d’un RécepteurConnection
+                Routeur FAI (bat admin)
+                Récepteur toit (IR ou Pont Wifi)
+                Récepteur sous sol (fibre ou cable)
+        Création d’un Local Technique pour chaque étage
+                Entre chaque local fibre optique multimode
+                2,5 prises ethernet/personne pour chaque étage
+     */
+
+
 
 }
 
 void NetworkBuilder::launchP4(Request request) {
+    /*
+     *Phase IV : Sous réseaux intra-bâtiment
+        Pour chaque étage i, bâtiment j
+                Mise en place d’un L2
+                Adresse du switch : 10.j.i.199
+                Adresse de broadcast : 10.j.i.255
+                Adresses disponibles pour DHCP : 10.j.i.200 - 10.j.i.254
+                Adresse privée du réseau : 10.j.i.0/24
+        Pour chaque bâtiment
+                Indiquer DHCP Relay : 10.j(admin).i(admin serveurs privés).10(arbitraire)
+                Indiquer Default Route : next hop : adresse privée router (coté intérieur du FW)
+     */
 
 }
