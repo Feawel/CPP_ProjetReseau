4b26ab3078d8add8b6f83280fca831d25149e588
Alexis Benoist <alexis.benoist@student.ecp.fr>
2013-11-23 17:01:19 +0100

    La fonction de génération dans un fichier texte commence à générer à partir de données mockups. Très sale, j'ai mis dans le main.

--- main.cpp
+++ main.cpp
@@ -9,12 +9,49 @@
 #include <QApplication>
 #include <QtGui>
 
+
+//mockup
+#include "phases/request.h"
+#include <vector>
+#include "model/location/building.h"
+#include "model/location/building_building.h"
+
+// basic file operations
+#include <iostream>
+#include <fstream>
+using namespace std;
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
     RequestInterface window ;
     window.show();
+
+
+    //mockup:
+    Building b1=Building("B1");
+    Building b2=Building("B2");
+    Building b3=Building("B3");
+
+    std::vector<Building*> bs;
+    bs.push_back(&b1);
+    bs.push_back(&b2);
+    bs.push_back(&b3);
+
+    ofstream myfile;
+    myfile.open ("example.txt");
+    myfile << "graph G {" << endl;
+
+    for(int ii=0; ii < bs.size(); ii++)
+    {
+        myfile << (*bs[ii]).getName() << endl;
+    }
+    myfile << "}" << endl;
+
+    myfile.close();
+
+
 
     return app.exec();
 }
